922c6d9bcdb8cc725df44bc97b6cad601300545d
Laura Sheach <laura_bubble_git@yahoo.com>
2019-05-27 16:22:28 +0100

    Remove unnecessaryworking code

--- models/rock_paper_scissors.rb
+++ models/rock_paper_scissors.rb
@@ -8,25 +8,6 @@ class RockPaperScissors
     @item2 = item2
   end
 
-
-  # def rock_wins(item1, item2)
-  #   if item1 == "rock" && item2 == "scissors"
-  #     return "Rock Wins!"
-  #   end
-  # end
-  #
-  # def scissors_wins(item1, item2)
-  #   if item1 == "scissors" && item2 == "paper"
-  #     return "Scissors Wins!"
-  #   end
-  # end
-  #
-  # def paper_wins(item1, item2)
-  #   if item1 == "paper" && item2 == "rock"
-  #     return "Paper Wins!"
-  #   end
-  # end
-
   def game_result
     result = case
     when @item1 == "rock" && @item2 == "scissors"
